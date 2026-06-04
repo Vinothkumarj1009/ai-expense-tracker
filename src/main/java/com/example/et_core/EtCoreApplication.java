03220b8ca778813968fe60ad0141806e55580ccf
Vinothkumar J <vinothkumarj1009@gmail.com>
2026-06-04 12:51:17 +0530

    Initial commit on develop

---
+++ src/main/java/com/example/et_core/EtCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.et_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtCoreApplication.class, args);
+	}
+
+}
